6eddb60ef96985c3bbb11adf4b1ae519d4e37313
Neil McWilliam <nmcwilliam@praxes.ca>
2023-07-25 00:06:25 -0300

    adjusted amplify line

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import awsmobile from './aws-exports';
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
