7447dbb95fa833d163debe56531bfe4013f89b1a
Neil McWilliam <nmcwilliam@praxes.ca>
2023-07-26 16:19:54 -0300

    updating createNote function

--- src/App.js
+++ src/App.js
@@ -46,17 +46,20 @@ const App = ({ signOut }) => {
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
     });
     fetchNotes();
     event.target.reset();
-  }
+  }  
 
   async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
