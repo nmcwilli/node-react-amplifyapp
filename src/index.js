264bd5fd2990aa261c31b2cda3c33bae47248111
Neil McWilliam <nmcwilliam@praxes.ca>
2023-07-28 00:15:56 -0300

    added apollo again

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from "@apollo/client" // Apollo integration
 Amplify.configure(config);
 
+const client = new ApolloClient({
+  uri: "https://awtbiphmbjefzla4jz4h37r67y.appsync-api.ca-central-1.amazonaws.com/graphql", 
+  cache: new InMemoryCache(), 
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
